6a172438787668b5f06fdf6ea88548f21c6d0053
Georgi Yanakiev <g.yanakiev@yahoo.com>
2021-03-23 15:56:28 +0200

    deletion from double linked list

--- SingleLinkedListProject/DoubleLinkedList.cs
+++ SingleLinkedListProject/DoubleLinkedList.cs
@@ -75,10 +75,99 @@ namespace SingleLinkedListProject
             }
         }
 
+        internal void InsertinEmptyList(int data)
+        {
+            throw new NotImplementedException();
+        }
+
         private void InsertInEmptyList(int data)
         {
             Node temp = new Node(data);
             start = temp;
+        }
+
+        public void DeleteFirstNode()
+        {
+            if (start == null) //  list is empty
+                return;
+            if (start.next == null)
+            {
+                start = null;
+                return;
+            }
+            start = start.next;
+            start.prev = null;
+        }
+
+        public void DeleteNode(int x)
+        {
+            if (start == null) //list is empty
+                return;
+            
+            
+            if (start.next == null) // list has only one node
+            {
+                if (start.info == x)
+                    start = null;
+                else
+                    Console.WriteLine(x + "not found");
+                return;
+            }
+            // Deletion of first node
+            if (start.info == x )
+            {
+                start = start.next;
+                start.prev = null;
+                return;
+            }
+
+            Node p = start.next;
+            while (p.next != null )
+            {
+                if (p.info == x)
+                    break;
+                p = p.next;
+            }
+
+            if ( p.next != null ) // node to be deleted is in between
+            {
+                p.prev.next = p.next;
+                p.next.prev = p.prev;
+            }
+            else // p refers to last node
+            {
+                if (p.info == x)
+                    p.prev.next = null;
+                else
+                    Console.WriteLine(x + " not found!");
+            }
+        }
+
+        public void DeleteLastNode(int data)
+        {
+            if (start == null)
+                return;
+
+            if (start.link == null)
+            {
+                start = null;
+                return;
+            }
+
+            Node p = start;
+            while (p.link.link != null)
+                p = p.link;
+            p.link = null;
+        }
+
+        internal void DeleteNode()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void InsertAtPosition(int data, int k)
+        {
+            throw new NotImplementedException();
         }
 
         public void InsertAfter(int data, int x)
@@ -102,6 +191,41 @@ namespace SingleLinkedListProject
                     p.next.prev = temp;
                 p.next = temp;
             }
+        }
+
+        internal void MergeSort()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void InsertCycle(int data)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool HasCycle()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void BubbleSortExLinks()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void RemoveCycle()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void BubbleSortExData()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ReverseList()
+        {
+            throw new NotImplementedException();
         }
 
         public void InsertBefore(int data, int x)
