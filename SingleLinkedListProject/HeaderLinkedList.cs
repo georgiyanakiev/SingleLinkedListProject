0261ba265ed11f5f6440675478c193c28155cf8c
Georgi Yanakiev <g.yanakiev@yahoo.com>
2021-04-03 19:55:46 +0300

    concatenate and headerLinkedList

---
+++ SingleLinkedListProject/HeaderLinkedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedListProject
+{
+    class HeaderLinkedList
+    {
+        private Node head;
+
+        public HeaderLinkedList()
+        {
+            head = new Node(0);
+        }
+
+        public void DisplayList()
+        {
+            if (head.link == null )
+            {
+                Console.WriteLine("List is empty\n");
+                return;
+            }
+            Node p = head.link;
+            Console.Write("List is :\n");
+            while (p != null )
+            {
+                Console.Write(p.info + " ");
+                p = p.link;
+            }
+            Console.WriteLine();
+        }
+    }
+}
